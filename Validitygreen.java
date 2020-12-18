bbfc1d5cc57844e4ca303e780ed6b11b0203b98c
rezwan737 <73266595+rezwan737@users.noreply.github.com>
2020-12-17 22:15:45 -0500

    Update Validitygreen.java

--- Validitygreen.java
+++ Validitygreen.java
@@ -26,10 +26,11 @@ System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
 		Thread.sleep(2000);
 		driver.findElement(By.id("mtscontact_message")).sendKeys("My favourite food is Biriyani");
 		Thread.sleep(2000);
-		
+		Thread.sleep(2000);
+		driver.findElement(By.id("mtscontact_submit")).click();
+		Thread.sleep(15000);
+		driver.quit();
 
 	}
 
 }
-
-//a[@href='https://validitygreen.com/contact-us/']
